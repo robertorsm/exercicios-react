1cfe0d265c23d83a699403546a3b9f4fb07d04ff
Roberto Sousa Martins <sroberto.rsm@gmail.com>
2020-02-27 21:17:31 -0300

    Primeiro Componente + Correção dependencias.

---
+++ src/componentes/PrimeiroComponente.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function primeiro() {
+  return <h1>Primeiro componente!</h1>;
+}
+
+export default primeiro;
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import PrimeiroComponente from './componentes/PrimeiroComponente';
+
+const elemento = document.getElementById('root');
+// ReactDOM.render(<h1>Olá React</h1>, elemento);
+ReactDOM.render(
+  <div>
+    <PrimeiroComponente />
+  </div>,
+  elemento,
+);
